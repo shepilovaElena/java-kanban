bddb75c9d040da0e2e278a6939dad38787076514
Shepilova <magic.hollow@yandex.kz>
2024-03-08 20:13:30 +0400

    sprint 5 version 1

---
+++ test/InMemoryHistoryTaskManagerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryHistoryTaskManagerTest {
+
+    @Test
+    void getHistory() {
+    }
+
+    @Test
+    void add() {
+    }
+}
